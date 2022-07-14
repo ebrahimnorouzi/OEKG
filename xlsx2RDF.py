df7be1ed8e80802c5680b77044d2ebb61b496432
Ebrahim Norouzi <ebrahim.norouzi@fiz-karlsruhe.de>
2022-07-14 10:24:15 +0200

    separate ontologies related to project and remove nan values.

--- xlsx2RDF.py
+++ xlsx2RDF.py
@@ -5,6 +5,11 @@ from rdflib import URIRef, BNode, Literal
 from rdflib import Namespace
 from rdflib.namespace import OWL, RDF, RDFS, FOAF, XSD
 import pandas as pd
+import zipfile
+from io import StringIO, BytesIO
+from urllib.request import urlopen
+from rdflib.plugin import register, Serializer, Parser
+register('text/rdf+n3', Parser, 'rdflib.plugins.parsers.notation3', 'N3Parser')
 
 df = pd.read_excel('MSE_ontologies.xlsx')
 
@@ -24,15 +29,18 @@ class xlsx2RDF(object):
         self.g.bind("pmd_kg", self.pmd_kg)
         
         # load excel file to dataframe
-        self.data = pd.read_excel('MSE_ontologies.xlsx')
+        self.data = pd.read_excel('MSE_ontologies.xlsx').fillna('')
     
         
     def create_triples(self):
 
         # bind the OMV namspaces
         omv = Namespace("http://omv.ontoware.org/2005/05/ontology#")
-        self.g.bind("omv", omv) 
-      
+        self.g.bind("omv", omv)
+
+        pmd_projects = URIRef(self.pmd_kg + 'pmd_projects')
+        self.g.add((pmd_projects, RDF.type, omv.Organisation))
+
         # iterate over excel file and create triples
         for row in self.data.itertuples(index=False):
             
@@ -46,21 +54,46 @@ class xlsx2RDF(object):
             ontology_domain = Literal(row[2], datatype=XSD.string)
             ontology_url = Literal(row[6], datatype=XSD.string)
 
-            ontology_n_classes = Literal(row[4], datatype=XSD.integer)
-            ontology_n_properties = Literal(row[5], datatype=XSD.integer)
-            
             # create triples
             self.g.add((ontology_short_name, RDF.type, omv.Ontology))
+            self.g.add((pmd_projects, omv.contributesToOntology, ontology_short_name))
+
+            if row[4] != '':
+                ontology_n_classes = Literal(row[4], datatype=XSD.integer)
+                self.g.add((ontology_short_name, omv.numClasses, ontology_n_classes))
+            if row[5] != '':
+                ontology_n_properties = Literal(row[5], datatype=XSD.integer)
+                self.g.add((ontology_short_name, omv.numProperties, ontology_n_properties))
 
             self.g.add((ontology_short_name, omv.name, ontology_name))
             self.g.add((ontology_short_name, omv.modificationDate, ontology_last_update))
             self.g.add((ontology_short_name, omv.description, ontology_domain))
             self.g.add((ontology_short_name, omv.resourceLocator, ontology_url))
-            self.g.add((ontology_short_name, omv.numClasses, ontology_n_classes))
-            self.g.add((ontology_short_name, omv.numProperties, ontology_n_properties))
             
+            # add all the used ontologies in an Ontology
+            if row[7] != '' and isinstance(row[7], str):
+                ontology_file = row[7].strip()
+                
+                if ontology_file[-3:] == "zip":
+                    myzipfile = zipfile.ZipFile(BytesIO(urlopen(ontology_file).read()))                    
+                    ontology_file = myzipfile.read('metal-alloy.owl')
+                    Ontology_format = 'xml'
+                
+                else:
+                    Ontology_format = 'xml' if ontology_file[-3:]=='owl' else ontology_file[-3:]
+
+                try:
+                    g = Graph().parse(ontology_file, format=Ontology_format)
+                    for prefix, namespace in g.namespaces():
+                        prefix = URIRef(self.pmd_kg + prefix)
+                        self.g.add((prefix, RDF.type, omv.Ontology))
+                        self.g.add((ontology_short_name, omv.useImports, prefix))
+                except:
+                    pass
+
             
         self.g.serialize(destination='mse_ontologies.ttl', format='ttl')
+
 
 
 
