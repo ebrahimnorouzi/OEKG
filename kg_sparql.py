5e8b5f819082eb6c97c95c41b068d5a264bf3045
Ebrahim Norouzi <ebrahim.norouzi@fiz-karlsruhe.de>
2022-07-22 10:46:30 +0200

    chord diagrams

--- kg_sparql.py
+++ kg_sparql.py
@@ -10,13 +10,15 @@ g = Graph()
 g.parse('mse_ontologies.ttl')
 
 query1 = """
-SELECT ?s 
+SELECT distinct ?s1 ?s2 ?q1 #(COUNT(?q1) AS ?count)
 WHERE {
-    pmd_kg:pmd_projects omv:contributesToOntology ?s .
-    ?s a omv:Ontology .
-    ?s omv:useImports pmd_kg:periodictable
+    pmd_kg:pmd_projects omv:contributesToOntology ?s1 .
+    ?s1 omv:useImports ?q1 .
+    ?s2 omv:useImports ?q1 .
+    FILTER(!(?s1=?s2) && ?q1 NOT IN (pmd_kg:))
 }
 """
+
 query2 = """
 SELECT ?q1 (COUNT(?q1) AS ?count)
 WHERE {
@@ -29,5 +31,5 @@ ORDER BY DESC(?count)
 qres = g.query(query2)
 
 for row in qres:
-    print(row)
+    print(row, '\n')
     #print(row['q1'], "frequency", row['count'])
