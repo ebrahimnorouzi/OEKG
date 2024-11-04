ba1d9acc00ede49f5da538394d8538be844acad3
ebrahimnorouzi <45043740+ebrahimnorouzi@users.noreply.github.com>
2024-11-04 16:43:06 +0100

    git error solved

--- csv2RDF.py
+++ csv2RDF.py
@@ -12,7 +12,9 @@ from urllib.parse import urlparse
 # Register the parser for N3 notation
 register('text/rdf+n3', Parser, 'rdflib.plugins.parsers.notation3', 'N3Parser')
 
+# Set GitHub token
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+headers = {"Authorization": f"token {GITHUB_TOKEN}"}
 
 class CSVtoRDF:
     def __init__(self):
@@ -41,67 +43,71 @@ class CSVtoRDF:
         self.data = pd.read_csv('MSE_ontologies.csv', sep=',', names=column_names, header=0, encoding_errors='ignore').fillna('')
 
     def fetch_github_metadata(self, url):
-        """Fetch metadata from a GitHub repository if URL is a GitHub link."""
+        """Fetch comprehensive metadata from a GitHub repository if URL is a GitHub link."""
         github_api_base = "https://api.github.com/repos"
         parsed_url = urlparse(url)
         path_parts = parsed_url.path.strip('/').split('/')
         
-        # Check if it's a GitHub URL and if it has the required parts
         if parsed_url.netloc != "github.com" or len(path_parts) < 2:
             return None
 
         owner, repo = path_parts[:2]
         api_url = f"{github_api_base}/{owner}/{repo}"
-        
-        # Define headers for authentication
-        headers = {"Authorization": f"token {GITHUB_TOKEN}"}
 
         try:
-            # Fetch repository data with authentication
-            response = requests.get(api_url, headers=headers)
-            
-            # Check if the response is valid
-            if response.status_code != 200:
-                print(f"Error fetching GitHub metadata for {owner}/{repo}: {response.json().get('message')}")
+            # Fetch main repository data
+            repo_response = requests.get(api_url, headers=headers)
+            if repo_response.status_code != 200:
+                print(f"Error fetching main repo data for {owner}/{repo}: {repo_response.json().get('message')}")
                 return None
-            
-            repo_data = response.json()
-            
-            # Fetch issues data
-            issues_data = requests.get(f"{api_url}/issues?state=all", headers=headers).json()
-            open_issues = len([issue for issue in issues_data if issue.get('state') == 'open'])
-            closed_issues = len(issues_data) - open_issues
+            repo_data = repo_response.json()
 
-            # Fetch branches data
-            branches_data = requests.get(f"{api_url}/branches", headers=headers).json()
-            branches = [branch.get('name') for branch in branches_data]
+            # Fetch additional metadata
+            languages_data = requests.get(f"{api_url}/languages", headers=headers).json()
+            topics_data = requests.get(f"{api_url}/topics", headers={**headers, "Accept": "application/vnd.github.mercy-preview+json"}).json()
+            contributors_data = requests.get(f"{api_url}/contributors", headers=headers).json()
+            releases_data = requests.get(f"{api_url}/releases", headers=headers).json()
+            tags_data = requests.get(f"{api_url}/tags", headers=headers).json()
+            issues_data = requests.get(f"{api_url}/issues?state=all", headers=headers).json()
+            commit_activity_data = requests.get(f"{api_url}/stats/commit_activity", headers=headers).json()
+            community_data = requests.get(f"{api_url}/community/profile", headers=headers).json()
 
             # Compile metadata
             metadata = {
                 "stars": repo_data.get("stargazers_count", 0),
                 "forks": repo_data.get("forks_count", 0),
-                "open_issues": open_issues,
-                "closed_issues": closed_issues,
+                "watchers_count": repo_data.get("watchers_count", 0),
+                "size": repo_data.get("size", 0),
+                "license": repo_data.get("license", {}).get("name"),
+                "has_wiki": repo_data.get("has_wiki", False),
+                "has_pages": repo_data.get("has_pages", False),
+                "default_branch": repo_data.get("default_branch"),
+                "topics": topics_data.get("names", []),
+                "languages": list(languages_data.keys()),
                 "last_update": repo_data.get("updated_at"),
-                "branches": branches,
-                "branch_count": len(branches),
                 "owner": repo_data.get("owner", {}).get("login"),
                 "description": repo_data.get("description"),
-                "language": repo_data.get("language")
+                "contributors": [{c.get("login"): c.get("contributions")} for c in contributors_data if "login" in c],
+                "tags": [tag.get("name") for tag in tags_data],
+                "releases": [{"tag_name": r.get("tag_name"), "published_at": r.get("published_at")} for r in releases_data],
+                "pulls_count": len([issue for issue in issues_data if issue.get("pull_request")]),
+                "open_issues": len([issue for issue in issues_data if issue.get("state") == 'open' and not issue.get("pull_request")]),
+                "closed_issues": len([issue for issue in issues_data if issue.get("state") == 'closed' and not issue.get("pull_request")]),
+                "commit_activity": commit_activity_data,
+                "community": {
+                    "health_percentage": community_data.get("health_percentage"),
+                    "has_code_of_conduct": bool(community_data.get("files", {}).get("code_of_conduct")),
+                    "has_contributing_guide": bool(community_data.get("files", {}).get("contributing")),
+                    "has_issue_template": bool(community_data.get("files", {}).get("issue_template")),
+                    "has_pull_request_template": bool(community_data.get("files", {}).get("pull_request_template"))
+                }
             }
-
-            # Fetch the commit count using the "commits" API
-            commits_response = requests.get(f"{api_url}/commits", headers=headers)
-            if commits_response.status_code == 200:
-                metadata["commits"] = len(commits_response.json())  # Approximate count based on current page
-            else:
-                metadata["commits"] = 0  # Fallback if API call fails
 
             return metadata
 
         except Exception as e:
             print(f"Error fetching GitHub metadata for {owner}/{repo}: {e}")
-            return None     
+            return None   
               
     def create_triples(self):
         """Convert CSV data to RDF triples and add them to the RDF graph."""
@@ -166,33 +172,90 @@ class CSVtoRDF:
             homepage = getattr(row, 'Homepage', '').strip()
             if "github.com" in homepage:
                 github_metadata = self.fetch_github_metadata(homepage)
-                
+
                 if github_metadata:
+                    # Basic repository metadata
                     stars = Literal(github_metadata['stars'], datatype=XSD.integer)
                     forks = Literal(github_metadata['forks'], datatype=XSD.integer)
+                    watchers_count = Literal(github_metadata['watchers_count'], datatype=XSD.integer)
+                    size = Literal(github_metadata['size'], datatype=XSD.integer)
                     last_update = Literal(github_metadata['last_update'], datatype=XSD.dateTime)
                     open_issues = Literal(github_metadata['open_issues'], datatype=XSD.integer)
                     closed_issues = Literal(github_metadata['closed_issues'], datatype=XSD.integer)
-                    branch_count = Literal(github_metadata['branch_count'], datatype=XSD.integer)
                     owner = Literal(github_metadata['owner'], datatype=XSD.string)
                     description = Literal(github_metadata['description'], datatype=XSD.string)
-                    language = Literal(github_metadata['language'], datatype=XSD.string)
-                    
-                    # Add GitHub metadata triples to the RDF graph
+                    license = Literal(github_metadata['license'], datatype=XSD.string)
+                    has_wiki = Literal(github_metadata['has_wiki'], datatype=XSD.boolean)
+                    has_pages = Literal(github_metadata['has_pages'], datatype=XSD.boolean)
+                    default_branch = Literal(github_metadata['default_branch'], datatype=XSD.string)
+
+                    # Add these triples to the RDF graph
                     self.g.add((ontology_short_name, self.oekg.stars, stars))
                     self.g.add((ontology_short_name, self.oekg.forks, forks))
+                    self.g.add((ontology_short_name, self.oekg.watchersCount, watchers_count))
+                    self.g.add((ontology_short_name, self.oekg.size, size))
                     self.g.add((ontology_short_name, self.oekg.lastUpdate, last_update))
                     self.g.add((ontology_short_name, self.oekg.openIssues, open_issues))
                     self.g.add((ontology_short_name, self.oekg.closedIssues, closed_issues))
-                    self.g.add((ontology_short_name, self.oekg.branchCount, branch_count))
                     self.g.add((ontology_short_name, self.oekg.owner, owner))
                     self.g.add((ontology_short_name, self.oekg.description, description))
-                    self.g.add((ontology_short_name, self.oekg.language, language))
-                    
-                    # Add branches as separate literals if needed
-                    for branch in github_metadata['branches']:
-                        branch_name = Literal(branch, datatype=XSD.string)
-                        self.g.add((ontology_short_name, self.oekg.branch, branch_name))
+                    self.g.add((ontology_short_name, self.oekg.license, license))
+                    self.g.add((ontology_short_name, self.oekg.hasWiki, has_wiki))
+                    self.g.add((ontology_short_name, self.oekg.hasPages, has_pages))
+                    self.g.add((ontology_short_name, self.oekg.defaultBranch, default_branch))
+
+                    # Topics
+                    for topic in github_metadata['topics']:
+                        topic_literal = Literal(topic, datatype=XSD.string)
+                        self.g.add((ontology_short_name, self.oekg.topic, topic_literal))
+
+                    # Languages
+                    for language in github_metadata['languages']:
+                        language_literal = Literal(language, datatype=XSD.string)
+                        self.g.add((ontology_short_name, self.oekg.language, language_literal))
+
+                    # Contributors
+                    for contributor in github_metadata['contributors']:
+                        for name, contributions in contributor.items():
+                            contributor_name = Literal(name, datatype=XSD.string)
+                            contributions_count = Literal(contributions, datatype=XSD.integer)
+                            self.g.add((ontology_short_name, self.oekg.contributor, contributor_name))
+                            self.g.add((ontology_short_name, self.oekg.contributions, contributions_count))
+
+                    # Tags
+                    for tag in github_metadata['tags']:
+                        tag_name = Literal(tag, datatype=XSD.string)
+                        self.g.add((ontology_short_name, self.oekg.tag, tag_name))
+
+                    # Releases
+                    for release in github_metadata['releases']:
+                        tag_name = Literal(release['tag_name'], datatype=XSD.string)
+                        published_at = Literal(release['published_at'], datatype=XSD.dateTime)
+                        self.g.add((ontology_short_name, self.oekg.releaseTag, tag_name))
+                        self.g.add((ontology_short_name, self.oekg.releaseDate, published_at))
+
+                    # Commit Activity
+                    for week_data in github_metadata['commit_activity']:
+                        week_total = Literal(week_data['total'], datatype=XSD.integer)
+                        self.g.add((ontology_short_name, self.oekg.weeklyCommits, week_total))
+
+                    # Community Health Metrics
+                    if github_metadata['community']:
+                        health_percentage = Literal(github_metadata['community'].get("health_percentage", 0), datatype=XSD.integer)
+                        self.g.add((ontology_short_name, self.oekg.healthPercentage, health_percentage))
+                        
+                        has_code_of_conduct = Literal(github_metadata['community'].get("has_code_of_conduct", False), datatype=XSD.boolean)
+                        self.g.add((ontology_short_name, self.oekg.hasCodeOfConduct, has_code_of_conduct))
+                        
+                        has_contributing_guide = Literal(github_metadata['community'].get("has_contributing_guide", False), datatype=XSD.boolean)
+                        self.g.add((ontology_short_name, self.oekg.hasContributingGuide, has_contributing_guide))
+                        
+                        has_issue_template = Literal(github_metadata['community'].get("has_issue_template", False), datatype=XSD.boolean)
+                        self.g.add((ontology_short_name, self.oekg.hasIssueTemplate, has_issue_template))
+                        
+                        has_pull_request_template = Literal(github_metadata['community'].get("has_pull_request_template", False), datatype=XSD.boolean)
+                        self.g.add((ontology_short_name, self.oekg.hasPullRequestTemplate, has_pull_request_template))
+
             '''
             # Process ontology file if provided
 
